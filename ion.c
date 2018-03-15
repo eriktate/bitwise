32e1a5b8099739efe3adc3a9ba5348347991ee65
eriktate <erik.tate.r@gmail.com>
2018-03-14 23:16:15 -0400

    Progress on recursive descent parser

--- ion.c
+++ ion.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
 
 #define MAX(x, y) ((x) >= (y) ? (x) : (y))
 
@@ -15,14 +18,14 @@ typedef struct BufHdr{
 	char buf[0]; // [0] new in C99
 } BufHdr;
 
-#define buf__hdr(b) ((BufHdr *)(char *)b - offsetof(BufHdr, buf))
+#define buf__hdr(b) ((BufHdr *)((char *)b - offsetof(BufHdr, buf)))
 #define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
 #define buf__fit(b, n) (buf__fits(b, n) ? 0 : ((b) = (buf__grow((b), buf_len(b) + (n), sizeof(*(b))))))
 
 #define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
 #define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
 #define buf_push(b, x) (buf__fit(b, 1), b[buf_len(b)] = (x), buf__hdr(b)->len++)
-#define buf_free(b) ((b) ? free(buf__hdr(b)) : 0)
+#define buf_free(b) ((b) ? (free(buf__hdr(b)), (b) = NULL) : 0)
 
 void *xrealloc(void *ptr, size_t num_bytes) {
 	ptr = realloc(ptr, num_bytes);
@@ -37,11 +40,24 @@ void *xrealloc(void *ptr, size_t num_bytes) {
 void *xmalloc(size_t num_bytes) {
 	void *ptr = malloc(num_bytes);
 	if (!ptr) {
-		perror("malloc failed");
+		perror("xmalloc failed");
 		exit(1);
 	}
 
 	return ptr;
+}
+
+void fatal(const char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+
+	printf("FATAL: ");
+	vprintf(fmt, args);
+	printf("\n");
+
+	va_end(args);
+
+	exit(1);
 }
 
 void *buf__grow(const void *buf, size_t new_len, size_t elem_size) {
@@ -74,6 +90,45 @@ void buf_test() {
 	buf_free(buf);
 }
 
+typedef struct InternStr {
+	size_t len;
+	const char *str;
+} InternStr;
+
+static InternStr *interns;
+
+const char *str_intern_range(const char *start, const char *end) {
+	size_t len = end - start;
+	for (size_t i = 0; i < buf_len(interns); i++) {
+		if (interns[i].len == len && strncmp(interns[i].str, start, len) == 0) {
+			return interns[i].str;
+		}
+	}
+
+	char *str = xmalloc(len + 1);
+	memcpy(str, start, len);
+	str[len] = 0;
+	buf_push(interns, ((InternStr){len, str}));
+
+	return str;
+}
+
+const char *str_intern(const char *str) {
+	return str_intern_range(str, str + strlen(str));
+}
+
+void str_intern_test() {
+	char x[] = "hello";
+	char y[] = "hello";
+	assert(x != y);
+	const char *px = str_intern(x);
+	const char *py = str_intern(y);
+	assert(px == py);
+	char z[] = "hello!";
+	const char *pz = str_intern(z);
+	assert(pz != px);
+}
+
 // lexing: translating char stream to token stream
 // e.g. 1234 (x+y) translates into '1234' '(' 'x' '+' 'y' ')'
 
@@ -85,20 +140,53 @@ typedef enum TokenKind {
 
 typedef struct Token {
 	TokenKind kind;
+	const char *start;
+	const char *end;
 	union {
-		uint64_t val;
-		struct {
-			const char *start;
-			const char *end;
-		};
+		int val;
+		const char *name;
 	};
 	// ...
 } Token;
 
+// Warning: This returns a pointer to a static internal buffer, so it'll be overwritten next call.
+const char *token_kind_name(TokenKind kind) {
+	char buf[256];
+	switch (kind) {
+	case TOKEN_INT:
+		sprintf(buf, "integer");
+		break;
+	case TOKEN_NAME:
+		sprintf(buf, "name");
+		break;
+	default:
+		if (kind < 128 && isprint(kind)) {
+			sprintf(buf, "%c", kind);
+		} else {
+			sprintf(buf, "<ASCII %d", kind);
+		}
+	}
+
+	return buf;
+}
+
 Token token;
 const char *stream;
 
+const char *keyword_if;
+const char *keyword_for;
+const char *keyword_while;
+
+void init_keywords() {
+	keyword_if = str_intern("if");
+	keyword_for = str_intern("for");
+	keyword_while = str_intern("while");
+}
+
+void print_token(Token token);
+
 void next_token() {
+	token.start = stream;
 	switch (*stream) {
 		case '0':
 		case '1':
@@ -110,11 +198,10 @@ void next_token() {
 		case '7':
 		case '8':
 		case '9': {
-			uint64_t val = 0;
+			int val = 0;
 			while (isdigit(*stream)) {
 				val *= 10;
 				val += *stream++ - '0';
-				stream++;
 			}
 			token.kind = TOKEN_INT;
 			token.val = val;
@@ -146,46 +233,51 @@ void next_token() {
 		case 'x':
 		case 'y':
 		case 'z':
-		// case 'a':
-		// case 'b':
-		// case 'c':
-		// case 'd':
-		// case 'e':
-		// case 'f':
-		// case 'g':
-		// case 'h':
-		// case 'i':
-		// case 'j':
-		// case 'k':
-		// case 'l':
-		// case 'm':
-		// case 'n':
-		// case 'o':
-		// case 'p':
-		// case 'q':
-		// case 'r':
-		// case 's':
-		// case 't':
-		// case 'u':
-		// case 'v':
-		// case 'w':
-		// case 'x':
-		// case 'y':
-		// case 'z':
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+		case 'G':
+		case 'H':
+		case 'I':
+		case 'J':
+		case 'K':
+		case 'L':
+		case 'M':
+		case 'N':
+		case 'O':
+		case 'P':
+		case 'Q':
+		case 'R':
+		case 'S':
+		case 'T':
+		case 'U':
+		case 'V':
+		case 'W':
+		case 'X':
+		case 'Y':
+		case 'Z':
 		case '_': {
 			const char *start = stream++;
 			while(isalnum(*stream) || *stream == '_') {
 				stream++;
 			}
 			token.kind = TOKEN_NAME;
-			token.start = start;
-			token.end = stream;
+			token.name = str_intern_range(token.start, stream);
 			break;
 		}
 		default:
 			token.kind = *stream++;
 			break;
 	}
+	token.end = stream;
+}
+
+void init_stream(const char *str) {
+	stream = str;
+	next_token();
 }
 
 void print_token(Token token) {
@@ -202,8 +294,122 @@ void print_token(Token token) {
 	printf("\n");
 }
 
+static inline bool is_token(TokenKind kind) {
+	return token.kind == kind;
+}
+
+static inline bool is_token_name(const char *name) {
+	return token.kind == TOKEN_NAME && token.name == name;
+}
+
+static inline bool match_token(TokenKind kind) {
+	if (is_token(kind)) {
+		next_token();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+static inline bool expect_token(TokenKind kind) {
+	if (is_token(kind)) {
+		next_token();
+		return true;
+	} else {
+		fatal("expected token %s, got %s", token_kind_name(kind), token_kind_name(token.kind));
+		return false;
+	}
+}
+
+/*
+
+expr3 = INT | '(' expr ')'
+expr2 = [-]expr3
+expr1 = expr2 ([/*]expr2)*
+expr0 = expr1 ([+-] expr1)*
+expr = expr0
+*/
+
+int parse_expr();
+
+int parse_expr3() {
+	if (is_token(TOKEN_INT)) {
+		int val = token.val;
+		next_token();
+		return val;
+	} else if (match_token('(')){
+		int val = parse_expr();
+		expect_token(')');
+		return val;
+	} else {
+		fatal("expected integer or (, got %s", token_kind_name(token.kind));
+	}
+}
+
+int parse_expr2() {
+	if (match_token('-')) {
+		return -parse_expr2();
+	} else {
+		return parse_expr3();
+	}
+}
+
+int parse_expr1() {
+	int val = parse_expr2();
+	while (is_token('*') || is_token('/')) {
+		char op = token.kind;
+		next_token();
+		int rval = parse_expr2();
+		if (op == '*') {
+			val *= rval;
+		} else {
+			assert(op = '/');
+			assert(rval != 0);
+			val /= rval;
+		}
+	}
+
+	return val;
+}
+
+int parse_expr0() {
+	int val = parse_expr1();
+	while (is_token('+') || is_token('-')) {
+		char op = token.kind;
+		next_token();
+		int rval = parse_expr1();
+		if (op == '+') {
+			val += rval;
+		} else {
+			assert(op == '-');
+			val -= rval;
+		}
+	}
+
+	return val;
+}
+
+int parse_expr() {
+	return parse_expr0();
+}
+
+int parse_expr_str(const char *expr) {
+	init_stream(expr);
+	return parse_expr();
+}
+
+#define TEST_EXPR(x) assert(parse_expr_str(#x) == (x))
+void parse_test() {
+	TEST_EXPR(1);
+	TEST_EXPR((1));
+	TEST_EXPR(1-2-3);
+	TEST_EXPR(2*3+4*5);
+	TEST_EXPR(2*(3+4)*5);
+	TEST_EXPR(2+-3);
+}
+
 void lex_test() {
-	char *source = "+()_HELLO1,234+FOO!994";
+	char *source = "XY+(XY)_HELLO1,234+FOO!994";
 	stream = source;
 	next_token();
 	while(token.kind) {
@@ -214,6 +420,8 @@ void lex_test() {
 
 int main(int argc, char **argv) {
 	buf_test();
-	// lex_test();
+	lex_test();
+	str_intern_test();
+	parse_test();
 	return 0;
 }
